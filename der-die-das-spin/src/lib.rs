f93e627ee45ca81794ecf37a109bb8b16537ec39
Ralf Anton Beier <ralf_beier@me.com>
2024-10-08 19:51:03 +0200

    fix(deps): Upgrade to newer spin and cargo deny
    Signed-off-by: Ralf Anton Beier <ralf_beier@me.com>

--- der-die-das-spin/src/lib.rs
+++ der-die-das-spin/src/lib.rs
@@ -1,11 +1,11 @@
 use serde::Serialize;
-use spin_sdk::sqlite::Connection;
+use spin_sdk::{http::Fields, sqlite::Connection};
 
 use {
     bindings::wasi::http::incoming_handler,
     futures::SinkExt,
     spin_sdk::{
-        http::{Fields, IncomingRequest, Method, OutgoingResponse, ResponseOutparam},
+        http::{IncomingRequest, Method, OutgoingResponse, ResponseOutparam},
         http_component,
     },
 };
@@ -15,9 +15,7 @@ mod bindings {
         path: "../spin-fileserver/examples/wit",
         world: "delegate",
         with: {
-            "wasi:http/types@0.2.0-rc-2023-10-18": spin_sdk::wit::wasi::http::types,
-            "wasi:io/streams@0.2.0-rc-2023-10-18": spin_sdk::wit::wasi::io::streams,
-            "wasi:io/poll@0.2.0-rc-2023-10-18": spin_sdk::wit::wasi::io,
+            "wasi:http/types@0.2.0": ::spin_sdk::wit::wasi::http::types,
         }
     });
 }
@@ -55,13 +53,14 @@ struct DerDieDas {
 async fn handle_request(request: IncomingRequest, response_out: ResponseOutparam) {
     match (request.method(), request.path_with_query().as_deref()) {
         (Method::Get, Some("/entry.json")) => {
-            let response = OutgoingResponse::new(
-                200,
-                &Fields::new(&[("content-type".to_string(), b"application/json".to_vec())]),
-            );
+            let fields =
+                Fields::from_list(&[("content-type".to_owned(), b"application/json".to_vec())])
+                    .unwrap();
+
+            let response = OutgoingResponse::new(fields);
 
             let mut body = response.take_body();
-
+            let _ = response.set_status_code(200);
             response_out.set(response);
             let b = h().await.unwrap();
 
@@ -76,7 +75,9 @@ async fn handle_request(request: IncomingRequest, response_out: ResponseOutparam
         }
 
         _ => {
-            response_out.set(OutgoingResponse::new(405, &Fields::new(&[])));
+            let response = OutgoingResponse::new(Fields::new());
+            response.set_status_code(405).unwrap();
+            response_out.set(response);
         }
     }
 }
